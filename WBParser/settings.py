896e7d44bb69d5d36ab2ea936a9eb093642baef7
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-06-26 01:33:14 +0500

    added swagger for documentation

--- WBParser/settings.py
+++ WBParser/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products',
     'rest_framework',
+    'drf_spectacular',
 ]
 
 # Rest Framework
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 MIDDLEWARE = [
